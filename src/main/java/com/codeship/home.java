c4ca37cb76385b97b6c24a1df2653f6f999cf7ed
vishal2082 <vishalbhardwaj070@gmail.com>
2018-06-07 13:25:24 +0530

    first commit

---
+++ src/main/java/com/codeship/home.java
@@ -0,0 +1,11 @@
+package com.codeship;
+
+public class home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to home...");
+	}
+
+}
